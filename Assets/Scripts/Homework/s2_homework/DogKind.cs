fa832451db0d93522fc8b767aae144e93f71406c
Ruby_liyuan.shen <ucbqlsh@ucl.ac.uk>
2018-10-26 01:37:16 +0100

    s2_homework
    attampt1

---
+++ Assets/Scripts/Homework/s2_homework/DogKind.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+
+public class DogKind {
+
+    int smallDogs = 1;
+    int mediumDogs = 2;
+    int largeDogs = 3;
+
+    public Dictionary<string, int> DogDictionary = new Dictionary<string, int>();
+
+    public void Start () {
+
+        DogDictionary.Add("Dachshouds", smallDogs);
+        DogDictionary.Add("Small Terries", smallDogs);
+        DogDictionary.Add("Bulldog", mediumDogs);
+        DogDictionary.Add("Whippet", mediumDogs);
+        DogDictionary.Add("Leonberger", largeDogs);
+        DogDictionary.Add("Greyhound", largeDogs);
+
+        if (DogDictionary.ContainsValue(1)) 
+        {
+            Console.WriteLine("it is a small dogs");
+        }
+
+        if (DogDictionary.ContainsValue(2))
+        {
+            Console.WriteLine("it is a medium dogs");
+        }
+
+        if (DogDictionary.ContainsValue(3))
+        {
+            Console.WriteLine("it is a large dogs");
+        }
+
+    }
+	
+}
---
+++ Assets/Scripts/Homework/s2_homework/Main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Main : DogKind{
+
+    List<string> Dogs = new List<string>();
+
+    public int numberOfDogs = Convert.ToInt32(Console.ReadLine());
+    public int numberOfhumans= Convert.ToInt32(Console.ReadLine());
+
+
+    void Evaluate () {
+        //fuction:evaluation relationship with human____________________________
+
+        if (numberOfDogs<=numberOfhumans )
+        {
+            Console.WriteLine("balenced relationship");
+        }
+        else
+        {
+            Console.WriteLine("unblenced relationship");
+        }
+
+        //list dog names________________________________________________________
+
+        Dogs.Add("John");
+        Dogs.Add("Niki");
+        Dogs.Add("Prince");
+        Dogs.Add("Sisi");
+    
+        int number = Dogs.Count - 1;
+
+        for (var i = 1; i <= number; i++)
+        {
+            Console.WriteLine("the name of dog is " + Dogs[i]);
+        }
+
+        //provide choice of dogkinds
+        bool needSmallDogs=Convert.ToBoolean(Console.ReadLine());
+        bool needMediumDogs= Convert.ToBoolean(Console.ReadLine());
+        bool needLargedogs= Convert.ToBoolean(Console.ReadLine());
+
+        if (needSmallDogs==true)
+        {
+            Console.WriteLine("dogKinds can be choosen is" + DogDictionary.ContainsValue(1));
+        }
+
+        if (needMediumDogs == true)
+        {
+            Console.WriteLine("dogKinds can be choosen is" + DogDictionary.ContainsValue(2));
+        }
+
+        if (needLargedogs == true)
+        {
+            Console.WriteLine("dogKinds can be choosen is" + DogDictionary.ContainsValue(3));
+        }
+
+    }
+
+
+}
