283f0a4790d7dacf1001da6572eb9de8b8e09fd5
Ruby_liyuan.shen <ucbqlsh@ucl.ac.uk>
2018-10-27 07:40:02 +0100

    s3 a1

---
+++ Assets/Scripts/Homework/s3_homework/moving.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour {
+
+    public GameObject cubePrefab;
+
+    public void  Start()
+    {
+        Instantiate(cubePrefab, new Vector3(0, 0, 0), Quaternion.identity, this.transform);
+    }
+
+    void update() {
+
+        foreach (Transform child in this.transform)
+        {
+            child.localPosition=new Vector3(Random.Range(0,10f), 0, 0);
+        }
+
+        foreach (Transform child in this.transform)
+        {
+            child.GetComponent<Light>().color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+
+
+}
